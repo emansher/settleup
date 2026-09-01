6384d3f405aea86883a90e8983c9901336eacf10
emansher <sa8570739@gmail.com>
2026-09-01 16:09:42 +0500

    Update api.js

--- frontend-js/src/lib/api.js
+++ frontend-js/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://settleup.bonto.run/api',
+ baseURL: 'https://settleup1.vercel.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
